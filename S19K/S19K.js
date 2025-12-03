64fd5a4bdf65c8dcb5633c2bebb2f7b09ec650ae
Evan <94743980+xzripper@users.noreply.github.com>
2025-12-04 00:17:14 +0100

    S19K

--- S19K/S19K.js
+++ S19K/S19K.js
@@ -34,6 +34,12 @@ export function addS19K() {
 export function setS19KActivity(active) {
   const s19kElement = document.getElementById("S19KElem");
 
+  if (!s19kElement) {
+    console.error("S19K: Can't change activity of non-existing S19K element.");
+
+    return;
+  }
+
   s19kElement.hidden = !active;
 }
 
@@ -43,6 +49,12 @@ export function setS19KActivity(active) {
  */
 export function removeS19K() {
   const s19kElement = document.getElementById("S19KElem");
+
+  if (!s19kElement) {
+    console.error("S19K: Can't destroy non-existing S19K element.");
+
+    return;
+  }
 
   s19kElement.remove();
 }
